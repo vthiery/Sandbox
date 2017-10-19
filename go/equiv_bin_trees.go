a035f1a739ecae0f79f0e4a185beb7fb6bf3fafe
Vincent Thiery <vjmthiery@gmail.com>
2017-10-19 22:50:23 +0200

    Compare trees

--- go/equiv_bin_trees.go
+++ go/equiv_bin_trees.go
@@ -23,14 +23,21 @@ func InternalWalk(t *tree.Tree, ch chan int) {
 // Same determines whether the trees
 // t1 and t2 contain the same values.
 func Same(t1, t2 *tree.Tree) bool {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	go Walk(t1, ch1)
+	go Walk(t2, ch2)
+	for i := range ch1 {
+		if i != <- ch2 {
+			return false
+		}
+	}
 	return true
 }
 
 func main() {
 	ch := make(chan int)
 	go Walk(tree.New(1), ch)
-	for v := range ch {
-		fmt.Print(v)
-	}
+	fmt.Println(Same(tree.New(1), tree.New(1)))
+	fmt.Println(Same(tree.New(1), tree.New(2)))
 }
-
