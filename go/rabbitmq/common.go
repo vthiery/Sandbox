c26c04bb54a7f66fe51c3b141e0f99459f6a39e0
Vincent Thiery <vjmthiery@gmail.com>
2017-10-21 21:28:32 +0200

    Cosmetic

--- go/rabbitmq/common.go
+++ go/rabbitmq/common.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"log"
-  )
+		"fmt"
+		"log"
+)
   
 func failOnError(err error, msg string) {
-	if err != nil {
-		log.Fatalf("%s: %s", msg, err)
-		panic(fmt.Sprintf("%s: %s", msg, err))
-	}
+		if err != nil {
+				log.Fatalf("%s: %s", msg, err)
+				panic(fmt.Sprintf("%s: %s", msg, err))
+		}
 }
